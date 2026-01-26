0c64ec114c3024a3dbaf1d40b0430e3bd5d7ee0e
AshirwadKumar45 <ashirwadkumar880@email.com>
2026-01-26 14:03:57 +0530

    Checkout Page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CheckoutPage from "./pages/user/Checkout";
+import UserDashboard from "./pages/user/Dashboard";
+
+const App = () => {
+  return <div></div>;
+};
+
+// export default App;
+export default CheckoutPage;
+// export default UserDashboard;
